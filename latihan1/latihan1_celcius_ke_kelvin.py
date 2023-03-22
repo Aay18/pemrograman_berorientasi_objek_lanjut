088e3f0d476ab2561c2cc20f83669410680c85e6
Aay18 <85240527+Aay18@users.noreply.github.com>
2023-03-22 12:29:17 +0700

    Add files via upload

---
+++ latihan1/latihan1_celcius_ke_kelvin.py
@@ -0,0 +1,3 @@
+C = 90
+K = C + 273
+print("konversi ",C, "derajat celcius adalah ",K, "derajat Kelvin")
---
+++ latihan1/latihan1_celcius_ke_reamur.py
@@ -0,0 +1,3 @@
+C = 60
+R = 4/5 * C
+print("konversi ",C, "derajat celcius adalah ",R, "derajat Reamur")
